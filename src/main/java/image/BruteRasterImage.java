8e1ae70a4f7e21a87d3dea9229350bcf06577c85
AgababyanArtur <artur.agababyan04@gmail.com>
2018-11-28 11:10:13 +0100

    Implémentation de 8 méthodes sur 9 Plus création de l'énumération RasterImageType

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -20,18 +20,50 @@ public class BruteRasterImage implements Image {
         this.colors = colors;
     }
 
+    //Les neufs méthodes de la classe
+
+    //Alloue la matrice représentant l'image
+    public void createRepresentation(){
+
+    }
+
+    //Fixe la couleur d'un pixel
+    public void setPixelColor(Color color, int x, int y){
+        colors[x][y] = color;
+    }
+
     @Override
     public Color getPixelColor(int x, int y) {
-        return null;
+        return colors[x][y];
+    }
+
+    //met à jour les valeurs de couleurs de l’image en utilisant les valeurs de la matrice donnée en paramètre
+    private void setPixelsColor(Color [][] pixels){
+        this.colors = pixels;
+    }
+
+    //les valeurs de tous les pixels pour qu’ils soient tous de la couleur donnée en paramètre
+    private void setPixelsColor(Color color){
+        for(int x = 0; x < height; x++)
+            for(int y = 0; y < width; y++)
+                colors[x][y] = color;
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        return width;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return height;
+    }
+
+    protected void setWidth(int width){
+        this.width = width;
+    }
+
+    protected void setHeight(int height){
+        this.height = height;
     }
 }
