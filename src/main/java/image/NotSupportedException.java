d506c882843f035e0999d7bcdf1078ddcff0a7bf
AgababyanArtur <artur.agababyan04@gmail.com>
2018-11-28 11:13:53 +0100

    Création classe NotSupportedException qui étend RuntimeException

---
+++ src/main/java/image/NotSupportedException.java
@@ -0,0 +1,8 @@
+package image;
+
+public class NotSupportedException extends RuntimeException {
+    private String message;
+    public NotSupportedException(String message){
+        this.message = message;
+    }
+}
