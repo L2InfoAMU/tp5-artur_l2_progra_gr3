7617fabc31c84833bc119ad9cb1220840e4d9e62
AgababyanArtur <artur.agababyan04@gmail.com>
2018-12-02 23:53:05 +0100

    correction des indices fin de la classe PaletteRasterImage

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -20,12 +20,9 @@ public class PaletteRasterImage implements Image {
     public PaletteRasterImage(Color color, int width, int height){
         this.width = width;
         this.height = height;
-        this.palette = new ArrayList<Color>();
-        this.indexesOfColors = new int[width][height];
+        createRepresentation();
         palette.add(color);
-        for(int x = 0; x < height; x++)
-            for(int y = 0; y < width; y++)
-                indexesOfColors[y][x] = palette.indexOf(color);
+        setPixelsColor(color);
     }
 
     //Constructeur 2
@@ -35,11 +32,56 @@ public class PaletteRasterImage implements Image {
         requiresNonNull(pixels);
         requiresNonZeroDimensions(pixels);
         requiresRectangularMatrix(pixels);
-        this.palette = new ArrayList<Color>();
+        createRepresentation();
+        setPixelsColor(pixels);
+    }
+
+    // Les 9 méthodes qui implémentent la classe PaletteRasterImage
+
+    //methode 1
+    public void createRepresentation(){
+        this.palette = new ArrayList<>();
         this.indexesOfColors = new int[width][height];
-        for(int x = 0; x < height; x++)
-            for (int y = 0; y < width; y++)
-                indexesOfColors[y][x] = palette.indexOf(pixels[y][x]);
+    }
+
+    //methode 2
+    public void setPixelColor(Color color, int x, int y){
+        if(!palette.contains(color))
+            palette.add(color);
+        indexesOfColors[x][y] = palette.indexOf(color);
+    }
+
+    //methode 3
+
+    public Color getPixelColor(int x, int y){
+        return palette.get(indexesOfColors[x][y]);
+    }
+
+    //methode 4
+    public void setPixelsColor(Color[][] pixels){
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                palette.add(pixels[x][y]);
+                indexesOfColors[x][y] = palette.indexOf(pixels[x][y]);
+            }
+        }
+    }
+
+    //methode 5
+    private void setPixelsColor(Color color){
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                indexesOfColors[x][y] = palette.indexOf(color);
+    }
+
+    //methode 6
+    public int getWidth() {
+        return width;
+    }
+
+    //methode 7
+    public int getHeight(){
+        return height;
     }
 
     //methode 8
